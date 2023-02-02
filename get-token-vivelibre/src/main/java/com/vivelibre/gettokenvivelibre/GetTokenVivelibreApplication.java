391996f73023db2ed404562354b3974e14380e83
Agustin Daniel Schüler Allub <agus.prog.development@gmail.com>
2023-02-02 21:05:08 +0100

    Init

---
+++ get-token-vivelibre/src/main/java/com/vivelibre/gettokenvivelibre/GetTokenVivelibreApplication.java
@@ -0,0 +1,13 @@
+package com.vivelibre.gettokenvivelibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetTokenVivelibreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetTokenVivelibreApplication.class, args);
+    }
+
+}
---
+++ get-token-vivelibre/src/test/java/com/vivelibre/gettokenvivelibre/GetTokenVivelibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivelibre.gettokenvivelibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTokenVivelibreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
